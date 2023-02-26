09b9647a104e07cd1cddd99240119c37f28d6aa5
Kerstin <kerstinweibeler@gmail.com>
2023-02-26 10:41:57 +0000

    feat: Setup Summernote

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['i-think-therefore-i-blog-kw.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['i-think-therefore-i-blog-kw.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
