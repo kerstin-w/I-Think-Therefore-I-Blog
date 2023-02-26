a49b4ca07c6076b9636896a6ad3e29dfd19d3b28
Kerstin <kerstinweibeler@gmail.com>
2023-02-26 17:25:22 +0000

    feat: Add comment form and view

--- codestar/settings.py
+++ codestar/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'django_summernote',
     'crispy_forms',
+    'crispy_bootstrap5',
     'blog',
 ]
 
@@ -61,7 +62,8 @@ LOGOUT_REDIRECT_URL = '/'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
